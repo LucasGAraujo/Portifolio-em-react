a052f535ddd74b0b2032a83f524a54abbd646299
Lucas Gomes <lucas.garaujo@al.infnet.edu.br>
2023-07-05 09:07:20 -0300

    finalizadofoo

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -10,7 +10,8 @@ function Sobre(){
             Olá! Me chamo Lucas Gomes busco por um estagio como desenvolvedor front-end. Nasci no ano de 2003, meu interesse pela programação venho bem do nada,
             conversando com minha prima ela tocou no assunto, meus olhos brilharam e resolvi pesquisar sobre. Atualmente tenho a honra de fazer parte da graduação de engenharia de software pela infnet.
             é fazer parte do grupo 4ºAndar. Também atualmente estou desenvolvendo um aplicativo com esse grupo. Atualmente, meu foco principal é evoluir por meu da programação e nunca deixar de adquirir conhecimento</p>      
-            <a href='https://github.com/LucasGAraujo' target={"_blank"} id="BUTON"><button className="button_animacao">Acesse meu gitHub</button></a>
+            <div><a href='https://github.com/LucasGAraujo' target={"_blank"} id="BUTON"><button className="button_animacao">Acesse meu gitHub</button></a>
+            <a href='https://expo.dev/@lucasgomes445?tab=snacks' target={"_blank"} id="BUTON"><button className="button_animacao">Acesse meu Expo</button></a></div>
          </>
     )
 }
